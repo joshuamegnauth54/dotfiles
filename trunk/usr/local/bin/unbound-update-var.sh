d15854fd17f76f84d47a6ba2adfd455cf7d2b7b9
Joshua Megnauth <joshuamegnauth54@gmail.com>
2021-08-18 11:58:07 -0400

    Continue paring down everything.

--- trunk/usr/local/bin/unbound-update-var.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script updates Unbound's root.key and root.hints.
-
-/usr/sbin/unbound-anchor -a /etc/unbound/var/root.key
-/usr/bin/curl -o /etc/unbound/var/root.hints https://www.internic.net/domain/named.root
-
-chown unbound:unbound /etc/unbound/var/root.key /etc/unbound/var/root.hints
-
