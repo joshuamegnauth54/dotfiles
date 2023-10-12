285c844c844f13d86a22ebedaaadf9bae0550549
Josh Megnauth <jo.sh@tutanota.com>
2023-10-11 23:31:47 -0400

    Update indent blankline to 3.0.

--- trunk/home/joshua/.config/nvim/lua/plugins/common.lua
+++ trunk/home/joshua/.config/nvim/lua/plugins/common.lua
@@ -38,9 +38,12 @@ return {
 	-- Indentation guidelines
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
 		opts = {
-			show_current_context = true,
-			show_current_context_start = true,
+			scope = {
+				enabled = true,
+				injected_languages = true,
+			},
 		},
 		event = "BufReadPre",
 	},
