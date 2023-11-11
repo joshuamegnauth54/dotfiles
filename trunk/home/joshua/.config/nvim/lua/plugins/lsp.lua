9ba4ce9d194a1e9af5f4ff35cd278a7e206329fe
Josh Megnauth <jo.sh@tutanota.com>
2023-11-10 23:14:04 -0500

    Add gamemode config and update neovim LSPs.

--- trunk/home/joshua/.config/nvim/lua/plugins/lsp.lua
+++ trunk/home/joshua/.config/nvim/lua/plugins/lsp.lua
@@ -142,6 +142,8 @@ return {
 				"mlir_pdll_lsp_server",
 				-- https://github.com/neomutt/mutt-language-server
 				"mutt_ls",
+				-- https://github.com/nushell/nushell
+				"nu",
 				-- https://github.com/Freed-Wu/pkgbuild-language-server
 				"pkgbuild_language_server",
 				-- I'm using this in conjunction with ruff.
